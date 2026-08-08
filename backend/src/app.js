419c7cd4a923812c71a234251391633442fd876d
tanusolanki-25 <tanusolanki-25 ke GitHub account ka verified email>
2026-08-08 20:01:04 +0530

    Some changes in file

--- backend/src/app.js
+++ backend/src/app.js
@@ -37,8 +37,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
 }))
 
-app.options("*", cors())
-
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
