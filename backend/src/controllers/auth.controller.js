53e018e82403843857c74a016d5adda382e3051a
tanusolanki-25 <tanusolanki-25 ke GitHub account ka verified email>
2026-08-09 14:27:36 +0530

    Some changes in file

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -102,7 +102,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true
+    secure: true,
+    sameSite: "none"
   }
 
   return res
@@ -128,7 +129,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true
+    secure: true,
+    sameSite: "none"
   }
 
   return res
@@ -166,9 +168,10 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 
     const options = {
-      httpOnly: true,
-      secure: true
-    }
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+  }
 
     const { accessToken, refreshToken: newRefreshToken } = await generateAccessAndRefreshToken(user.id)
 
