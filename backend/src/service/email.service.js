5e3dd58083dd72067aa0d273bf2e0e0463f7b516
tanusolanki-25 <tanusolanki-25 ke GitHub account ka verified email>
2026-08-13 17:37:02 +0530

    new changes

--- backend/src/service/email.service.js
+++ backend/src/service/email.service.js
@@ -1,41 +1,41 @@
-import nodemailer from 'nodemailer'
-import { ApiError } from '../utils/ApiError.js'
+import nodemailer from "nodemailer";
+import { ApiError } from "../utils/ApiError.js";
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth:{
-    type: 'OAuth2',
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
     user: process.env.GOOGLE_USER,
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-  }
-})
+  },
+});
 
-transporter.verify((error, success)=>{
-  if(error){
-    console.error('Error connecting to gmail server')
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("GMAIL VERIFY ERROR:", error);
+  } else {
+    console.log("Email server is ready to send message");
   }
-  else{
-    console.log('Email server is ready to send message')
-  }
-})
+});
 
-export const sendEmail = async (to, subject, text, html) =>{
-  try{
-     const info = await transporter.sendMail({
+export const sendEmail = async (to, subject, text, html) => {
+  try {
+    const info = await transporter.sendMail({
       from: `"HealthConnect" <${process.env.GOOGLE_USER}>`,
       to,
       subject,
       text,
       html,
-     })
+    });
 
-     console.log('Message sent: %s', info.messageId)
-     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
-  }
-  catch(error){
-    console.log('Error sending email:', error)
+    console.log("Message sent:", info.messageId);
+  } catch (error) {
+    console.error("EMAIL SEND ERROR:", error);
+    console.error("ERROR MESSAGE:", error.message);
+    console.error("ERROR CODE:", error.code);
+
     throw new ApiError(500, "Unable to send verification email");
   }
-}
+};
