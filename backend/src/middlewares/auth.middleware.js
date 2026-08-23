d83e29d62cff920fc8f958d69fdc4ffd54407513
tanusolanki-25 <tanusolanki-25 ke GitHub account ka verified email>
2026-08-23 17:55:08 +0530

    new changes

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -10,7 +10,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
-      throw new ApiError(401, "Unauthorized access");
+      return next();
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
