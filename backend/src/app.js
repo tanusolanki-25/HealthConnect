db16977d156cf78c0f4bf8c28057dd59b1382c3b
tanusolanki-25 <tanusolanki-25 ke GitHub account ka verified email>
2026-08-24 15:52:58 +0530

    new changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -41,6 +41,7 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+console.log(process.env.COOKIE_KEY);
 app.use(session({
   secret: process.env.COOKIE_KEY,
   resave: false,
