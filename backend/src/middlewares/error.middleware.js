b9af46a1249821b50f1d27feb7373c56ed4aff4a
tanusolanki-25 <tanusolanki-25 ke GitHub account ka verified email>
2026-08-24 16:18:50 +0530

    new changes

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,23 +1,30 @@
 import { ApiError } from "../utils/ApiError.js";
 
 const errorHandler = (err, req, res, next) => {
-  let error = err;
-
-  if (!(error instanceof ApiError)) {
-    const statusCode = error.statusCode || 500;
-    const message = error.message || "Internal Server Error";
-    error = new ApiError(statusCode, message, error?.errors || [], err.stack);
+  try {
+    let error = err;
+  
+    if (!(error instanceof ApiError)) {
+      const statusCode = error.statusCode || 500;
+      const message = error.message || "Internal Server Error";
+      error = new ApiError(statusCode, message, error?.errors || [], err.stack);
+    }
+  
+    const response = {
+      statusCode: error.statusCode,
+      message: error.message,
+      success: false,
+      errors: error.errors,
+      ...(process.env.NODE_ENV === "development" ? { stack: error.stack } : {}),
+    };
+  
+    return res.status(error.statusCode).json(response);
+  } catch (error) {
+    console.error(error);
+  console.error(error.message);
+  console.error(error.stack);
+  throw error;
   }
-
-  const response = {
-    statusCode: error.statusCode,
-    message: error.message,
-    success: false,
-    errors: error.errors,
-    ...(process.env.NODE_ENV === "development" ? { stack: error.stack } : {}),
-  };
-
-  return res.status(error.statusCode).json(response);
 };
 
 export { errorHandler };
